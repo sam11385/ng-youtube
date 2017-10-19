50193476c53c12c603bff09bde026f9b791fe032
sam11385 <samluedke@gmail.com>
2017-10-19 11:53:31 -0400

    First commit! load up material and necessary angular stuff. Create observable for getting api results and search box to query youtube API.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+const API_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = 'AIzaSyCRJqo_zdv1gDIsSkczJOFTnKcm2coSWEA';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app works!';
+  searchForm: FormGroup;
+  results: Observable<{}>;
+
+  constructor(private formBuilder: FormBuilder, private http: Http) {
+    this.searchForm = this.formBuilder.group({
+      search: ['', Validators.required],
+    });
+
+    this.results = this.searchForm.controls.search.valueChanges
+      .filter(value => value.length > 2)
+      .debounceTime(500)
+      .distinctUntilChanged()
+      .switchMap(query =>
+        this.http.get(
+          `${API_URL}?q=${query}&key=${API_KEY}&maxResults=20&part=snippet&type=video`
+        )
+      )
+      .map(res => res.json().items);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatCardModule,
+  MatGridListModule,
+} from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatGridListModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
